9c4d66913f7c9b45cd00a66175c11d4df2db39c3
liusihan-单位 <evaseemefly@126.com>
2017-07-26 12:54:53 +0800

    1 搭建.net mvc框架 2 添加对mysql的orm模型映射

---
+++ NFMS/NFMS.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFMS.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ NFMS/NFMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NFMS.Startup))]
+namespace NFMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
