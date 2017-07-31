67d1013323aa444c32376ad07abb439422888fa0
easyou <826168013@qq.com>
2017-07-31 08:34:50 +0800

    78787878

--- NFMS/NFMS/Controllers/AccountController.cs
+++ NFMS/NFMS/Controllers/AccountController.cs
@@ -18,6 +18,9 @@ namespace NFMS.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+        /// <summary>
+        /// ghghghghgh
+        /// </summary>
         public AccountController()
         {
         }
