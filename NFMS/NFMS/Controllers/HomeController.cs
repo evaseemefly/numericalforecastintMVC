1ec90e9e44f0c5a03bd59bfca9c83f7f0b6e6201
liusihan-单位 <evaseemefly@126.com>
2017-08-07 17:14:53 +0800

    8月7日修改

--- NFMS/NFMS/Controllers/HomeController.cs
+++ NFMS/NFMS/Controllers/HomeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NFMS.IBLL;
+using NFMS.BLL;
 
 namespace NFMS.Controllers
 {
     public class HomeController : Controller
     {
+        IactioninfoBLL actionBLL = new actioninfoBLL();
+
         public ActionResult Index()
         {
+            actionBLL.GetListBy(a=>(a.isShow==true)&&(a.DelFlag==false))
             return View();
         }
 
