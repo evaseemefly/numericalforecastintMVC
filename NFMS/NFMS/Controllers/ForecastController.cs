32dd238bf1d1149ddfc2bcd67310fa7077d7093a
liusihan-单位 <evaseemefly@126.com>
2017-08-11 16:54:57 +0800

    8月11日 完成布局页 添加knockout 实现部分动态绑定功能

---
+++ NFMS/NFMS/Controllers/ForecastController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NFMS.Controllers
+{
+    public class ForecastController : Controller
+    {
+        // GET: Forecast
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectMapping()
+        {
+            return View();
+        }
+    }
+}
