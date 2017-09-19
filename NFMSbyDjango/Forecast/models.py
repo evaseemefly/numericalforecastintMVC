cea207d096d25fef7d4c8f6f4ea4f88f0fbc2a1f
liusihan-单位 <evaseemefly@126.com>
2017-09-19 12:58:10 +0800

    9月19日 单位电脑

--- NFMSbyDjango/Forecast/models.py
+++ NFMSbyDjango/Forecast/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class ActionInfo(models.Model):
+    # 主键
+    AID=models.AutoField(primary_key=True)
+    ParentID=models.IntegerField
+    Name=models.CharField(max_length=64)
+    DelFlag=models.BooleanField(default=True)
+    ModifiedOnTime=models.DateField
+    Remark=models.CharField(max_length=256)
+    Url=models.CharField(max_length=256)
+    AreaName=models.CharField(max_length=64)
+    ActionMethodName=models.CharField(max_length=64)
+    ControllerName=models.CharField(max_length=64)
+    JsFunctionName=models.CharField(max_length=32)
+    Sort=models.SmallIntegerField
+    ActionTypeEnum=models.PositiveSmallIntegerField
+    IconWidth=models.PositiveSmallIntegerField
+    IconHeigh=models.PositiveSmallIntegerField
+    IconCls=models.CharField(max_length=64)
+    IconClassName=models.CharField(max_length=64)
+    isShow=models.BooleanField(default=True)
+    MethodTypeEnum=models.PositiveSmallIntegerField
--- NFMSbyDjango/NFMSbyDjango/settings.py
+++ NFMSbyDjango/NFMSbyDjango/settings.py
@@ -82,9 +82,18 @@ WSGI_APPLICATION = 'NFMSbyDjango.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nfsysbydj',
+        'USER': 'root',
+        'PASSWORD': 'icanflyeva',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
