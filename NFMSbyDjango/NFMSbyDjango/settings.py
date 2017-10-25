85a9a2ffa429c896f755d8b0c1ab30464e805ec8
liusihan-单位 <evaseemefly@126.com>
2017-10-25 17:51:06 +0800

    10月25日 完成部分内容

--- NFMSbyDjango/NFMSbyDjango/settings.py
+++ NFMSbyDjango/NFMSbyDjango/settings.py
@@ -215,6 +215,7 @@ FTP_URL="128.5.6.21"
 # TARGET_DIR="D:\测试"
 # TARGET_DIR="E:\\03协同开发\\数值预报显示系统\\numericalforecastintMVC\\NFMSbyDjango\\static\\img\\download"
 TARGET_DIR=os.path.join(BASE_DIR,'static','img','download')
+print("TARGET_DIR:%s"%TARGET_DIR)
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 # MEDIA_URL = '/media/'
 
